ed97834fb2a81c5593b6bcee418047628cbecd34
Alexandre Sobolevski <sobolevski.a@gmail.com>
2019-01-07 08:53:47 -0500

    Inject fetch dependency

--- test.js
+++ test.js
@@ -1,18 +1,35 @@
 const fetch = require('node-fetch');
+const assert = require('assert');
+const sinon = require('sinon');
 const { makeCachedFetch, cleanCache } = require('./lib');
 
-const cachedFetch = makeCachedFetch();
+
+const testUrl = 'https://api.thedogapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1';
+let stubbedFetch;
+let realResponse;
+let cachedFetch;
 
 describe('saves to cache request', () => {
+  before(async() => {
+    realResponse = await fetch(testUrl);
+    stubbedFetch = sinon.stub().returns(Promise.resolve(realResponse));
+    stubbedFetch.Response = fetch.Response;
+    cachedFetch = makeCachedFetch(stubbedFetch);
+  });
+
   after(async () => {
     await cleanCache();
   });
 
-  it('sets the key the requested path', async () => {
-    const response = await cachedFetch('https://api.thedogapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1');
+  it('uses cache if it exists', async () => {
+    // request #1 should hit the real api
+    const response = await cachedFetch(testUrl);
     const body = await response.json();
-    const response2 = await cachedFetch('https://api.thedogapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1');
+    // request #2 should access the cache
+    const response2 = await cachedFetch(testUrl);
     const body2 = await response2.json();
     // TODO: do actual verification here
+    assert.deepStrictEqual(body, body2, 'both body requests are the same');
+    assert.equal(stubbedFetch.calledOnce, true, 'fetch was called only once');
   });
 });
